e89e5e01241895baf8a5a8ddffb1d4c5aeb5b141
Natashia White <whitenat@oregonstate.edu>
2019-11-17 16:07:02 -0800

    update main.go to use templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-//Use the http.NewServeMux() function to initialize a new servemux
-//then register the home function as the handler for the "/" URL pattern
-//Finally, register the 2 new handler functions and URL patterns
+	//Use the http.NewServeMux() function to initialize a new servemux
+	//then register the home function as the handler for the "/" URL pattern
+	//Finally, register the 2 new handler functions and URL patterns
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/navigation", showNavigation)
 	//mux.HandleFunc("/snippet/create", createSnippet)
 
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-//Use the http.ListenAndServe() function to start a new web server
-//We pass in 2 params: the TCP network address to liston on, and the
-//servemux we just created. If http.ListenAndServe() returns an error
-//that any error return by http.ListenAndServe() is always non-nil
+	http.HandleFunc("/", serveTemplate)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+
+	//Use the http.ListenAndServe() function to start a new web server
+	//We pass in 2 params: the TCP network address to liston on, and the
+	//servemux we just created. If http.ListenAndServe() returns an error
+	//that any error return by http.ListenAndServe() is always non-nil
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
