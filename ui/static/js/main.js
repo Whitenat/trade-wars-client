0309a57ee13c41d911aac0d5c020909706fb4825
Marji Symonds <symondsm@oregonstate.edu>
2019-11-19 11:20:49 -0800

    Remove js and jquery

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
 
 });
-
-function getName() {
-	var username = $("#username").val()
-	window.location.href='/navigation'
-	document.getElementById("showUser").innerHTML = username;
-}
