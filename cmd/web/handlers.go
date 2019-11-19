db1157c072490602711d3d0ade16ffb08091513b
Natashia White <whitenat@oregonstate.edu>
2019-11-18 18:17:37 -0800

    add handler for chat page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -119,3 +119,36 @@ func showTrade(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
+
+//Navigation handler function which serves the chat page
+func showChat(w http.ResponseWriter, r *http.Request) {
+	//Check if URL path matches exactly
+	if r.URL.Path != "/chat" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/chat.page.tmpl",
+	}
+
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// We then use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents any
+	// dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
