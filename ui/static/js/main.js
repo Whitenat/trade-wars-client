aa42ea3a9d61487cd8fdd118a9cbd58fb5c73e14
Marji Symonds <symondsm@oregonstate.edu>
2019-11-18 19:11:11 -0800

    Add new js file

---
+++ ui/static/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	function getName() {
+		var username = $("#username").val()
+		window.location.href='/navigation'
+		document.getElementById("showUser").innerHTML = username;
+	}
+});
