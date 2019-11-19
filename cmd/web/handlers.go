7d30af10716cdf9f7d189ca3f88cf594b580791f
Marji Symonds <symondsm@oregonstate.edu>
2019-11-18 17:44:23 -0800

    Move navigation from html to templ

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,7 +44,35 @@ files := []string{
 
 //Add a showNavigation handler function
 func showNavigation(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+//Check if URL path matches exactly
+	if r.URL.Path != "/navigation" {
+		http.NotFound(w, r)
+		return
+	}
+
+files := []string{
+	"./ui/html/navigation.page.tmpl",
+	}
+
+// Use the template.ParseFiles() function to read the template file into a
+// template set. If there's an error, we log the detailed error message and use
+// the http.Error() function to send a generic 500 Internal Server Error
+// response to the user.
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+ 	}
+
+// We then use the Execute() method on the template set to write the template
+// content as the response body. The last parameter to Execute() represents any
+// dynamic data that we want to pass in, which for now we'll leave as nil.
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 // //Add a createSnippet handler function
